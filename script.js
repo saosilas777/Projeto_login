ea0c284457d895d8576d9e5ba3ef78a585d5f071
Silas Dias <saosilas@hotmail.com>
2023-02-28 17:40:28 -0300

    projeto login

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var pwd = document.getElementById('pwd').value;
+var user = document.getElementById('name').value;
+function validation() {
+
+    if (pwd === "12345" && user === "Silas") {
+        document.getElementById('btn').style.opacity = '1';
+    }
+    else {
+        document.getElementById('btn').style.opacity = '0';
+    }
+
+}
+
+setInterval(() => {
+    validation();
+}, 1);
+
