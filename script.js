959aea7f6e65803faf5140fe3f7dac7eab7a74b7
Silas Dias <71517241+saosilas777@users.noreply.github.com>
2023-02-28 18:03:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var user = document.getElementById('name').value;
 function validation() {
 
     if (pwd === "12345") {
-        document.getElementById('btn').style.opacity = '1';
+        document.getElementById('btn').style.display = 'block';
     }
     else {
-        document.getElementById('btn').style.opacity = '0';
+        document.getElementById('btn').style.display = 'none';
     }
 
 }
